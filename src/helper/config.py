6223cd62cc7aff9811980ca83bed8f9310403b64
telkomdev-sidik <sdkhadiwijaya@gmail.com>
2022-04-09 12:27:46 +0700

    change structure

--- src/helper/config.py
+++ src/helper/config.py
@@ -2,10 +2,10 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = ''
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = ''
-app.config['MYSQL_DATABASE_HOST'] = ''
+app.config['MYSQL_DATABASE_USER'] = 'myrubber_python'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'BukasaJ4'
+app.config['MYSQL_DATABASE_DB'] = 'myrubber_calculation'
+app.config['MYSQL_DATABASE_HOST'] = '103.145.226.120'
 mysql.init_app(app)
 
 
